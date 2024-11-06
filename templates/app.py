e1e9410d888430a552cd0a5a3310e0309638418d
Kelin Argueta <kelinargueta@icloud.com>
2024-11-06 10:11:12 -0500

    no message

--- templates/app.py
+++ templates/app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 db_config = {
     'host': ' localhost', 
     'user': 'root ',
-    'password': 'Honduras504',
+    'password': 'your password',
     'database': 'System_monitoring'
 }
 
